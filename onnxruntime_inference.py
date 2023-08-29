335282e057d55843834e43516361a007652731f6
Besserwisser1 <einBesserwisser@yandex.ru>
2023-08-29 16:43:44 +0700

    test results

--- onnxruntime_inference.py
+++ onnxruntime_inference.py
@@ -14,7 +14,7 @@ model_name = 'yolov8n-seg'
 model_path = "./model"
 ONNX_MODEL = f"{model_path}/{model_name}-{input_height}-{input_width}.onnx"
 video_path = "test.mp4"
-video_inference = False
+video_inference = True
 CLASSES = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis','snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
 
 sess = onnxruntime.InferenceSession(ONNX_MODEL)
